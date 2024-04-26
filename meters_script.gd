b4c6d97ff675cd9ca333973c37ba4f94e187e66f
tnguy510 <tnguy510@ucsc.edu>
2024-04-26 13:35:22 -0700

    starting work on meters

--- meters_script.gd
+++ meters_script.gd
@@ -1,11 +1,32 @@
-extends Node
-
+extends ProgressBar
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var musicArray = ["Vocaloid", "Country", "Pop", "Rock", "Kpop", "Jpop"]
+var wantedMusic = "Country"
+
+var enjoyment = 0 : set = _set_enjoyment
+
+func _set_enjoyment(new_enjoyment):
+	var prev_enjoyment = enjoyment
+	enjoyment = min(max_value, new_enjoyment)
+	value = enjoyment
+	
+	if enjoyment <= 0:
+		queue_free()
+		
+	#if enjoyment < prev_enjoyment:
+		
+# Called when the node enters the scene tree for the first time.
+func init_meter(_enjoyment):
+	enjoyment = _enjoyment
+	max_value = enjoyment
+	value = enjoyment
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _on_timer_timeout():
+	#pick out a new wantedMusic from music Array Needs to be attached
+	wantedMusic = musicArray[randi() % 6]
+	print(wantedMusic)
 	pass
+	
+
