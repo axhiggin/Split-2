ad69da6cd0e027ce3a3912e41b46aca311976d77
LeCodingOwl <nguyenjimmy627@gmail.com>
2024-05-02 13:44:39 -0700

    Added radio selection

--- car.gd
+++ car.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 var volumeLevel = 50
+var radioStation = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
@@ -17,11 +18,25 @@ func _on_volume_up_interacted(body):
 	volumeLevel += 10
 	if volumeLevel > 100:
 		volumeLevel = 100
-	print("Volume " + str(volumeLevel))
+	print("Volume: " + str(volumeLevel))
 
 
 func _on_volume_down_interacted(body):
 	volumeLevel -= 10
 	if volumeLevel < 0:
 		volumeLevel = 0
-	print("Volume " + str(volumeLevel))
+	print("Volume: " + str(volumeLevel))
+
+
+func _on_change_radio_up_interacted(body):
+	radioStation += 1
+	if radioStation > 10:
+		radioStation = 0
+	print("Radio station: " + str(radioStation))
+
+
+func _on_change_radio_down_interacted(body):
+	radioStation += 1
+	if radioStation < 0:
+		radioStation = 10
+	print("Radio station: " + str(radioStation))
