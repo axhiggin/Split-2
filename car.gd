97e34d5e3d56aeeebd121326f3cfb3ffb621841b
LeCodingOwl <nguyenjimmy627@gmail.com>
2024-05-03 07:55:52 -0700

    Deleted my old code in car.gd
    Just removed the signal calls I made before Trish's changes

--- car.gd
+++ car.gd
@@ -12,31 +12,3 @@ func _ready():
 func _process(delta):
 	#moves the car
 	self.position += Vector3(0, 0, 1)
-
-#These get called from the buttons node signals
-func _on_volume_up_interacted(body):
-	volumeLevel += 10
-	if volumeLevel > 100:
-		volumeLevel = 100
-	print("Volume: " + str(volumeLevel))
-
-
-func _on_volume_down_interacted(body):
-	volumeLevel -= 10
-	if volumeLevel < 0:
-		volumeLevel = 0
-	print("Volume: " + str(volumeLevel))
-
-#Not sure how we plan on implantmenting the radio but here's a simple system to change the radio
-func _on_change_radio_up_interacted(body):
-	radioStation += 1
-	if radioStation > 10:
-		radioStation = 0
-	print("Radio station: " + str(radioStation))
-
-
-func _on_change_radio_down_interacted(body):
-	radioStation -= 1
-	if radioStation < 0:
-		radioStation = 10
-	print("Radio station: " + str(radioStation))
